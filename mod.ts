0e8b9fae1a52ca45c86a66b690b3a475627ba3de
Brett Earle <brettearle@gmail.com>
2024-04-16 22:12:29 +0930

    added doc comments to both days between and days since

--- mod.ts
+++ mod.ts
@@ -2,6 +2,11 @@ interface DateAsNumberObj {
   day: number;
 }
 
+/**
+ * Returns the number of days since the given date.
+ * Throws an error if the date is in the future.
+ * Throws an error if the input is not a Date object.
+ */
 export const daysSince = (date: Date): number => {
   if (!(date instanceof Date)) {
     throw new Error("Not a Date object");
@@ -16,6 +21,10 @@ export const daysSince = (date: Date): number => {
   return daysSince;
 };
 
+/**
+ * This returns the difference in days between two dates.
+ * Regardless of the order of the dates, the result will always be positive.
+ */
 export const daysBetween = (date1: Date, date2: Date): number => {
   const dateOne: DateAsNumberObj = {
     day: date1.getDate(),
